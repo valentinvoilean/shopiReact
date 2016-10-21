42e9032d81081572c73547abcbf6023e5dc8e9de
Valentin Voilean <valentin.voilean@netcentric.biz>
2016-10-21 16:36:47 +0200

    updated SAVE_HEADER_SETTINGS reducer

--- src/utils/updateHeaderComponentPosition.js
+++ src/utils/updateHeaderComponentPosition.js
@@ -1,12 +1,20 @@
 export default function(initialState, newData) {
     const componentName = Object.keys(newData)[0];
     const componentProperties = newData[componentName];
-    let newSettings = {};
 
-    newSettings[componentName] = {
-        ...initialState[componentName],
-        ...componentProperties
-    };
+    if (componentName && componentProperties) {
+        let newSettings = {};
 
-    return {...initialState, ...newSettings};
+        newSettings[componentName] = {
+            ...initialState[componentName],
+            ...componentProperties
+        };
+
+        return {...initialState, ...newSettings};
+    }
+
+    else {
+        console.warn('Component\'s name or its properties are not defined.');
+        return initialState;
+    }
 }
