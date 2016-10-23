5590cade42d59bac7efaa48356097aa36a7e121a
Valentin Voilean <valentin.voilean@netcentric.biz>
2016-10-23 18:05:52 +0200

    updated validation

--- src/utils/validateState.js
+++ src/utils/validateState.js
@@ -25,6 +25,12 @@ export const validateState = state => {
     let wantedState = {...state},
         newState = {},
         components,
+        validComponents,
+
+        _loadDefaultSettings = (mediaQuery) => {
+            console.warn(`Conditions not met; default settings will be loaded.`);
+            newState[mediaQuery] = {...defaultState.HeaderConfig[mediaQuery]};
+        },
 
         _checkCondition = (component, mediaQuery, fullCondition) => {
             const conditionArray = fullCondition.split('-'),
@@ -42,47 +48,57 @@ export const validateState = state => {
 
         _parseEachComponent = (mediaQuery) => {
             // Go trough each component
-            for (let component in components) {
+            for (let component in validComponents) {
                 if (components.hasOwnProperty(component)) {
-                    const wantedPosition = components[component].position,
-                        availablePositions = validStates[mediaQuery][component];
-                    let conditionMet = false;
 
-                    for (let i = 0; i < availablePositions.length; i++) {
-                        const availablePositionAndCondition = availablePositions[i].split('|'),
-                            availablePosition = availablePositionAndCondition[0],
-                            fullCondition = availablePositionAndCondition[1];
+                    if (wantedState[mediaQuery].hasOwnProperty(component)) {
+                        const wantedPosition = components[component].position,
+                            availablePositions = validStates[mediaQuery][component];
+                        let conditionMet = false;
 
-                        if (wantedPosition === availablePosition) {
+                        for (let i = 0; i < availablePositions.length; i++) {
+                            const availablePositionAndCondition = availablePositions[i].split('|'),
+                                availablePosition = availablePositionAndCondition[0],
+                                fullCondition = availablePositionAndCondition[1];
 
-                            if (typeof fullCondition !== 'undefined') {
-                                conditionMet = _checkCondition(component, mediaQuery, fullCondition);
+                            if (wantedPosition === availablePosition) {
+
+                                if (typeof fullCondition !== 'undefined') {
+                                    conditionMet = _checkCondition(component, mediaQuery, fullCondition);
+                                }
+                                else {
+                                    newState[mediaQuery][component] = {...components[component]};
+                                    conditionMet = true;
+                                }
+
+                                break;
                             }
-                            else {
-                                newState[mediaQuery][component] = {...components[component]};
-                                conditionMet = true;
-                            }
+                        }
 
+                        if (!conditionMet) { // return default settings if the current property not valid
+                            _loadDefaultSettings(mediaQuery);
                             break;
                         }
                     }
-
-                    if (!conditionMet) {
-                        console.warn(`Conditions not met; default settings will be loaded.`);
-                        newState[mediaQuery] = {...defaultState.HeaderConfig[mediaQuery]};
-                        break;
-                    }
+                }
+                else { // return default settings if a component name is missing
+                    _loadDefaultSettings(mediaQuery);
+                    break;
                 }
             }
         };
 
     return (() => {
         // Go trough each media query
-        for (let mediaQuery in wantedState) {
+        for (let mediaQuery in validStates) {
             if (wantedState.hasOwnProperty(mediaQuery)) {
                 newState[mediaQuery] = newState[mediaQuery] ? newState[mediaQuery] : {};
                 components = {...wantedState[mediaQuery]};
+                validComponents = {...validStates[mediaQuery]};
                 _parseEachComponent(mediaQuery);
+            }
+            else { // return default settings for the current media query
+                _loadDefaultSettings(mediaQuery);
             }
         }
 
