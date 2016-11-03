08982c52407109baf8d55a6f034be2ac3baeb8a4
Valentin Voilean <valentin.voilean@netcentric.biz>
2016-11-03 20:56:48 +0100

    updated structure

--- src/js/react/HeaderConfigApp/reducers/modalReducer.js
+++ src/js/react/HeaderConfigApp/reducers/modalReducer.js
@@ -1,16 +1,18 @@
+import { includes } from 'lodash';
+
 import { mediaQueries } from 'HeaderConfigApp/constants/mediaQueries';
 import { SAVE_HEADER_SETTINGS, REMOVE_HEADER_ITEM } from 'HeaderConfigApp/constants/actionTypes';
 import { getInitialState, validateState } from 'HeaderConfigApp/utils/modalUtil';
 
 // Reducer
-export default (state = getInitialState(), action = {}) => {
+export default (state = getInitialState(), action) => {
     switch (action.type) {
         case SAVE_HEADER_SETTINGS:
         case REMOVE_HEADER_ITEM:
             const mediaQuery = Object.keys(action.payload)[0];
             const newPositions = action.payload[mediaQuery];
 
-            if (mediaQueries.indexOf(mediaQuery) !== -1 && newPositions) {
+            if (includes(mediaQueries, mediaQuery) && newPositions) {
                 return Object.assign({}, state, {
                     [mediaQuery]: {
                         ...state[mediaQuery],
@@ -20,8 +22,10 @@ export default (state = getInitialState(), action = {}) => {
             }
             else {
                 console.warn('Component\'s name or its properties are not defined.');
-                return state;
+                return validateState(state);
             }
+
+            break;
 
         default:
             return validateState(state);
