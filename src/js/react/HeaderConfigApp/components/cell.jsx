5d4b4541f4d9135ba82aaf5c7857f434a7373d3c
Valentin Voilean <valentin.voilean@netcentric.biz>
2016-11-05 16:24:30 +0100

    updated cell

--- src/js/react/HeaderConfigApp/components/cell.jsx
+++ src/js/react/HeaderConfigApp/components/cell.jsx
@@ -1,66 +1,89 @@
-import { includes } from 'lodash';
-
 import React from 'react';
-import Sortable from 'react-sortablejs';
+import ReactDOM from 'react-dom';
+import Sortable from 'sortablejs';
+import {includes} from 'lodash';
 
 import styles from 'HeaderConfigApp/styles/modal.scss';
 
 // Functional Component
-const Cell = props => {
-    const {items, name, actions, mediaQuery} = props;
-
-    let sortable = null; // sortable instance
-
-    const sortableOptions = {
-        group: { name: name, put: () => true },
-        animation: 150,
-        ghostClass: styles.sortableGhost,
-        onEnd({to, from}) {
-            actions.save({
-                [mediaQuery]: {
-                    [to.className]: [...to.children].map(item => item.dataset.id),
-                    [from.className]: [...from.children].map(item => item.dataset.id)
-                }
-            });
-        },
-        onMove({to, dragged}) {
-            const newList = [...to.children].map((item) => item.dataset.id);
-            const draggedItem = dragged.dataset.id;
-
-            if (draggedItem === 'Logo' && newList.length > 0) {
-                return false;
-            }
-
-            return !includes(newList, 'Logo');
-        }
+class Cell extends React.Component {
+    static propTypes = {
+        items: React.PropTypes.object,
+        name: React.PropTypes.string,
+        actions: React.PropTypes.object,
+        remove: React.PropTypes.func,
+        mediaQuery: React.PropTypes.string
     };
 
-    const itemsHTML = items[name]
-        ? items[name].map((item, key) => (
+    sortable = null; // sortable instance
+
+    constructor(props) {
+        super(props);
+
+        const {items, name, actions, mediaQuery} = props;
+
+        this.sortableOptions = {
+            group: {name: 'headerConfig'},
+            animation: 150,
+            ghostClass: styles.sortableGhost,
+            onEnd({to, from}) {
+                actions.save({
+                    [mediaQuery]: {
+                        [to.className]: [...to.children].map(item => item.dataset.id),
+                        [from.className]: [...from.children].map(item => item.dataset.id)
+                    }
+                });
+            },
+            onMove({to, dragged}) {
+                const newList = [...to.children].map((item) => item.dataset.id);
+                const draggedItem = dragged.dataset.id;
+
+                if (draggedItem === 'Logo' && newList.length > 0) {
+                    return false;
+                }
+
+                return !includes(newList, 'Logo');
+            }
+        };
+
+        this.itemsHTML = items[name]
+            ? items[name].map((item, key) => (
             <li key={key} data-id={item}>{item} <span className={styles.close} onClick={() => actions.remove({
                 items, item, mediaQuery,
                 positionLists: sortable.el.children,
                 position: sortable.el.className
             })}>&#10005;</span></li>)
-         ) : '';
+        ) : '';
 
-    return (
-        <Sortable
-            className={name}
-            tag="ul"
-            options={sortableOptions}
-            ref={(c) => { if (c) { sortable = c.sortable; } }}>
-            {itemsHTML}
-        </Sortable>
-    );
-};
+        this.state = {
+            hoverClass: ''
+        };
+    }
 
-Cell.propTypes = {
-    items: React.PropTypes.object,
-    name: React.PropTypes.string,
-    actions: React.PropTypes.object,
-    remove: React.PropTypes.func,
-    mediaQuery: React.PropTypes.string
-};
+    componentDidMount() {
+        /*ReactDOM.findDOMNode(this).addEventListener('dragenter', () => {
+            this.setState({hoverClass: ' test'});
+        });
+
+        ReactDOM.findDOMNode(this).addEventListener('dragleave', () => {
+            this.setState({hoverClass: ''});
+        });*/
+
+        this.sortable = Sortable.create(ReactDOM.findDOMNode(this), {...this.sortableOptions});
+    }
+
+    componentWillUnmount() {
+        if (this.sortable) {
+            this.sortable.destroy();
+            this.sortable = null;
+        }
+    }
+
+    render() {
+        return (
+            <ul className={this.props.name}> {this.itemsHTML} </ul>
+        );
+    }
+}
 
 export default Cell;
