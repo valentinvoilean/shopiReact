d54a2586bdf6cbfc485bdca47acca216040a25bb
Valentin Voilean <valentin.voilean@netcentric.biz>
2016-11-11 16:28:36 +0100

    updated karma config

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,6 @@
-// Karma configuration
-// Generated on Fri Feb 19 2016 15:10:40 GMT-0500 (EST)
+import webpack from 'webpack';
+import path from 'path';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
 export default {
     browsers: ['PhantomJS'],
@@ -14,9 +15,36 @@ export default {
     },
     webpack: { //kind of a copy of your webpack config
         devtool: 'inline-source-map', //just do inline source maps instead of the default
+        resolve: {
+            extensions: ['', '.js', '.jsx', '.svg'],
+            modulesDirectories: ['src/js', 'node_modules'],
+            alias: {
+                'HeaderConfigApp': 'react/HeaderConfigApp',
+                'HeaderApp': 'react/HeaderApp',
+                'jquery': 'jquery/dist/jquery.min.js',
+                modernizr$: path.resolve(__dirname, '.modernizrrc') }
+        },
+        plugins: [
+            new webpack.ProvidePlugin({
+                $: 'jquery',
+                jQuery: 'jquery'
+            }),
+            new ExtractTextPlugin('helpers.css', {allChunks: false})
+        ],
         module: {
+            noParse: ['jquery'],
             loaders: [
-                { test: /\.jsx$/, loader: 'babel-loader' }
+                {test: /\.jsx?$/, include: `${__dirname}/src/js`, loader: 'babel-loader'},
+                {test: /\.svg$/, loader: 'svg-sprite'},
+                {test: /\.modernizrrc$/, loader: 'modernizr'},
+                {test: /\.eot(\?v=\d+.\d+.\d+)?$/, loader: 'file'},
+                {test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url?limit=10000&mimetype=application/font-woff'},
+                {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
+                {test: /\.(jpe?g|png|gif)$/i, loader: 'file?name=[name].[ext]'},
+                {test: /\.ico$/, loader: 'file?name=[name].[ext]'},
+                {test: /\.less$/, loader: ExtractTextPlugin.extract('style', 'css!autoprefixer!less')},
+                {test: /\.scss$/, loader: ExtractTextPlugin.extract('style', 'css?sourceMap&modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!autoprefixer!resolve-url!sass?sourceMap')},
+                {test: /\.css$/, loader: ExtractTextPlugin.extract('style', 'css')}
             ]
         }
     },
--- src/js/react/HeaderConfigApp/containers/modal.spec.jsx
+++ src/js/react/HeaderConfigApp/containers/modal.spec.jsx
@@ -1,4 +1,7 @@
+import ReactTestUtils from 'react-addons-test-utils';
+
 describe('root', function () {
+
     it('should work out of the box', function () {
         expect(true).toBeTruthy();
     });
