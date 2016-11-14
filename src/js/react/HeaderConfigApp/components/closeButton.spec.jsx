1cb4a9adc5796dc6a97587faa7f57882544f623e
Valentin Voilean <valentin.voilean@netcentric.biz>
2016-11-14 16:32:35 +0100

    updated karma config

--- src/js/react/HeaderConfigApp/components/closeButton.spec.jsx
+++ src/js/react/HeaderConfigApp/components/closeButton.spec.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import TestUtils from 'react-addons-test-utils';
+
 import CloseButton from './closeButton.jsx';
 
 describe('root', function () {
 
+    let spy = jasmine.createSpy('test');
     let closeButton = TestUtils.renderIntoDocument(
-        <CloseButton onClick={()=> void 0} item="Menu"/>
-    );
-    /*
-    let span = TestUtils.findRenderedDOMComponentWithTag(
-        closeButton, 'span'
+        <div><CloseButton onClick={spy} item="Menu"/></div>
     );
 
-    it('renders without problems', function () {
-        expect(span.getDOMNode().textContent)
-            .toEqual('&#10005;');
-    });*/
+    it('renders without problems', function() {
+        expect(ReactDOM.findDOMNode(closeButton).querySelector('span').textContent).toBe('✕');
+    });
 
-    it('shoud be true', function() {
-        expect(true).toBeTruthy();
+    it('it should call the spy method on click', function() {
+        const statelessComponent = TestUtils.findDOMNode(closeButton).children[0];
+
+        TestUtils.Simulate.click(statelessComponent);
+        expect(spy).toHaveBeenCalled();
     });
 });
