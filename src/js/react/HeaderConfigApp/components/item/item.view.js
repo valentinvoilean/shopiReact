f414822799700baae6d73cd87fae6963ba907fdc
Valentin Voilean <valentin.voilean@netcentric.biz>
2016-11-22 11:01:45 +0100

    implemented dragsource on item

--- src/js/react/HeaderConfigApp/components/item/item.view.js
+++ src/js/react/HeaderConfigApp/components/item/item.view.js
@@ -1,11 +1,34 @@
 import React, {PropTypes} from 'react';
+import {ItemTypes} from 'HeaderConfigApp/constants/itemTypes';
+import {DragSource} from 'react-dnd';
 
 import {CloseButtonView} from 'HeaderConfigApp/components';
 
-const ItemVIew = ({item, name, onClick, mediaQuery}) => {
+const itemSource = {
+    beginDrag(props) {
+        console.log(props);
+        return {};
+    }
+};
 
-    return (
-        <li><span>{item}</span>
+const collect = (connect, monitor) => {
+    return {
+        connectDragSource: connect.dragSource(),
+        isDragging: monitor.isDragging()
+    };
+};
+
+const ItemVIew = (props) => {
+    const {connectDragSource, isDragging, item, name, onClick, mediaQuery} = props;
+
+    console.log(isDragging);
+
+    return connectDragSource(
+        <li style={{
+            opacity: isDragging ? 0.5 : 1,
+            cursor: 'move'
+        }}
+        ><span>{item}</span>
             <CloseButtonView cellName={name}
                              item={item}
                              onClick={onClick}
@@ -19,7 +42,9 @@ ItemVIew.propTypes = {
     item: PropTypes.string.isRequired,
     onClick: PropTypes.func.isRequired,
     mediaQuery: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired
+    name: PropTypes.string.isRequired,
+    connectDragSource: PropTypes.func.isRequired,
+    isDragging: PropTypes.bool.isRequired
 };
 
-export default ItemVIew;
+export default DragSource(ItemTypes.COMPONENT, itemSource, collect)(ItemVIew);
