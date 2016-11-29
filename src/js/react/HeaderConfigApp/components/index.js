412917b47ba26ef45c7e04cb35f15783304abd53
Valentin Voilean <valentin.voilean@netcentric.biz>
2016-11-29 18:40:08 +0100

    updated modal

--- src/js/react/HeaderConfigApp/components/index.js
+++ src/js/react/HeaderConfigApp/components/index.js
@@ -1,8 +1,8 @@
 export TabContainer from './tab/tab.container';
 export CellContainer from './cell/cell.container';
 export CodeContainer from './code/code.container';
-export ModalContainer from './modal/modal.container';
 
+export ModalView from './modal/modal.view';
 export RowView from './row/row.view';
 export CloseButtonView from './closeButton/closeButton.view';
 export CodeView from './code/code.view';
