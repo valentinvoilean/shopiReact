836b0931e7356cb7278fc49588c06316f55fff58
Valentin Voilean <valentin.voilean@netcentric.biz>
2017-01-04 17:14:25 +0100

    updated webpack (HotModuleReplacementPlugin - sass)

--- webpack.config.js
+++ webpack.config.js
@@ -2,36 +2,46 @@ const webpack = require('webpack');
 const path = require('path');
 //const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-const GLOBALS = {
-    'process.env.NODE_ENV': JSON.stringify('development'),
-    __DEV__: true
-};
-
 module.exports = {
-    devtool: 'source-map',
-
+    devtool: 'inline-source-map',
+    context: path.resolve(__dirname, '.'),
     entry: {
-        vendors: ['babel-polyfill', 'modernizr', 'picturefill', 'react', 'react-dom', 'react-match-media',
-            'react-redux', 'redux', 'jquery'],
+        vendors: [
+            'babel-polyfill',
+            'modernizr',
+            'picturefill',
+            'react',
+            'react-dom',
+            'react-match-media',
+            'react-redux',
+            'redux',
+            'jquery'
+        ],
         config: 'App/features/HeaderConfig/index.jsx',
         main: 'App/index.js'
     },
-
     output: {
         path: path.resolve(__dirname, 'theme/assets'),
         publicPath: '/theme/assets/',
-        filename: '[name].js'
+        filename: '[name].js',
+        chunkFilename: '[name].js'
     },
-
-    devServer: {
-        inline: true,
-        hot: true,
-        noInfo: true,
-        compress: true,
-        port: 8081,
-        open: true
+    module: {
+        noParse: /jquery|backbone/,
+        rules: [
+            {enforce: 'pre', test: /\.jsx?$/, include: `${__dirname}/src/js`, loader: 'eslint-loader'},
+            {test: /\.jsx?$/, include: `${__dirname}/src/js`, loader: 'babel-loader'},
+            {test: /\.svg$/, loader: 'svg-sprite-loader'},
+            {test: /\.modernizrrc$/, loader: 'modernizr-loader!json-loader'},
+            {test: /\.eot(\?v=\d+.\d+.\d+)?$/, loader: 'file-loader'},
+            {test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader?limit=10000&mimetype=application/font-woff'},
+            {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=application/octet-stream'},
+            {test: /\.(jpe?g|png|gif)$/i, loader: 'file-loader?name=[name].[ext]'},
+            {test: /\.ico$/, loader: 'file-loader?name=[name].[ext]'},
+            {test: /\.scss$/, loader: 'style-loader!css-loader?modules&importLoaders=2&sourceMap&localIdentName=[name]__[local]___[hash:base64:5]!autoprefixer-loader!resolve-url-loader!sass-loader?outputStyle=expanded&sourceMap'},
+            {test: /\.css$/, loader: 'style-loader!css-loader'}
+        ]
     },
-
     resolve: {
         extensions: ['.js', '.jsx', '.svg'],
         modules: [
@@ -43,22 +53,21 @@ module.exports = {
             modernizr$: path.resolve(__dirname, '.modernizrrc')
         }
     },
-
-    target: 'web',
-
     plugins: [
-        new webpack.DefinePlugin(GLOBALS),
-        new webpack.NamedModulesPlugin(),
         new webpack.HotModuleReplacementPlugin(),
-        new webpack.optimize.CommonsChunkPlugin({name: 'vendors', filename: 'vendors.js'}),
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify('development'),
+            __DEV__: true
+        }),
+        new webpack.NamedModulesPlugin(),
         new webpack.ProvidePlugin({
             $: 'jquery',
             jQuery: 'jquery'
         }),
         /*new ExtractTextPlugin({
-            filename: 'helpers.css',
-            allChunks: false
-        }),*/
+         filename: 'helpers.css',
+         allChunks: false
+         }),*/
         new webpack.LoaderOptionsPlugin({
             test: /\.jsx/, // may apply this only for some modules
             options: {
@@ -69,92 +78,14 @@ module.exports = {
             }
         })
     ],
-
-    module: {
-        noParse: /jquery|backbone/,
-        rules: [
-            {enforce: 'pre', test: /\.jsx?$/, include: `${__dirname}/src/js`, loader: 'eslint-loader'},
-            {test: /\.jsx?$/, include: `${__dirname}/src/js`, loader: 'babel-loader'},
-            {test: /\.svg$/, loader: 'svg-sprite-loader'},
-            {test: /\.modernizrrc$/, loader: ['modernizr-loader', 'json-loader']},
-            {test: /\.eot(\?v=\d+.\d+.\d+)?$/, loader: 'file-loader'},
-            {
-                test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-                loader: 'url-loader',
-                query: {
-                    limit: 10000,
-                    mimetype: 'application/font-woff'
-                }
-            },
-            {
-                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-                loader: 'url-loader',
-                query: {
-                    limit: 10000,
-                    mimetype: 'application/octet-stream'
-                }
-            },
-            {
-                test: /\.(jpe?g|png|gif)$/i,
-                loader: 'file-loader',
-                query: {
-                    name: '[name].[ext]'
-                }
-            },
-            {
-                test: /\.ico$/, loader: [{
-                loader: 'file-loader',
-                query: {
-                    name: '[name].[ext]'
-                }
-            }]
-            },
-            {
-                test: /\.scss$/,
-                loader: [
-                    'style-loader',
-                    {
-                        loader: 'css-loader',
-                        query: {
-                            sourceMap: true,
-                            modules: true,
-                            importLoaders: true,
-                            localIdentName: '[name]__[local]___[hash:base64:5]'
-                        }
-                    },
-                    'autoprefixer-loader',
-                    'resolve-url-loader',
-                    'sass-loader'
-                ]
-                /*loader: ExtractTextPlugin.extract({
-                    fallbackLoader: 'style-loader',
-                    loader: [
-                        {
-                            loader: 'css-loader',
-                            query: {
-                                sourceMap: true,
-                                modules: true,
-                                importLoaders: true,
-                                localIdentName: '[name]__[local]___[hash:base64:5]'
-                            }
-                        },
-                        'autoprefixer-loader',
-                        'resolve-url-loader',
-                        'sass-loader'
-                    ]
-                })*/
-            },
-            {
-                test: /\.css$/,
-                loader: ['style-loader', 'css-loader']
-                /*loader: ExtractTextPlugin.extract({
-                    fallbackLoader: 'style-loader',
-                    loader: 'css-loader'
-                })*/
-            }
-        ]
+    devServer: {
+        inline: true,
+        hot: true,
+        compress: true,
+        port: 8081,
+        open: true
     },
-
+    target: 'web',
     performance: {
         hints: false
     }
