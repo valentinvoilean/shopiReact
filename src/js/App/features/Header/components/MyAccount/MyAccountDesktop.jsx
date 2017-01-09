d1255c92cb99d47602514a39ad96b6566606b244
Valentin Voilean <valentin.voilean@netcentric.biz>
2017-01-09 16:28:14 +0100

    updated MyAccount

--- src/js/App/features/Header/components/MyAccount/MyAccountDesktop.jsx
+++ src/js/App/features/Header/components/MyAccount/MyAccountDesktop.jsx
@@ -1,5 +1,8 @@
 import React, {Component} from 'react';
 import md5 from 'md5';
+import classnames from 'classnames';
+
+import {SHARED_CLASSES} from 'common/constants/classes';
 
 export default class MyAccountDesktop extends Component {
 
@@ -11,47 +14,97 @@ export default class MyAccountDesktop extends Component {
                 first_name : 'Vali', // eslint-disable-line
                 email: 'vali.voilean@gmail.com'
             },
+            shop: {
+                customer_accounts_optional: true // eslint-disable-line
+            },
 
-            loggedIn: false
+            loggedIn: false,
+
+            isHiddenSideCollapsed: false,
+            isHiddenSideOutsideViewport: true
         };
+    }
+
+    componentDidMount() {
+        this.calculateWidths();
     }
 
     shouldComponentUpdate() {
         return true;
     }
 
-    render() {
-        const loggedInHtml = (
-            <div className="myAccount__visibleSide">
-                <a href="/account" className="myAccount__img">
-                    <div className="myAccount__gravatar">
-                        <img alt="avatar" src={`//gravatar.com/avatar/${md5(this.state.customer.email)}?s=40&d=blank`} />
-                    </div>
-                    <svg className="myAccount__icon">
-                        <use xlinkHref="#user-2"></use>
-                    </svg>
-                </a>
+    calculateWidths() {
+        this.setState({
+            hiddenSideWidth: this.hiddenSide.offsetWidth,
+            welcomeMessageWidth: this.state.loggedIn ? this.welcomeMessage.offsetWidth : null,
+            isHiddenSideCollapsed: true,
+            isHiddenSideOutsideViewport: false
+        });
+    }
 
-                <a className="myAccount__link is-active" href="/account">Hi, {this.state.customer.first_name} !</a>
+    render() {
+        const hiddenSideClasses = classnames('myAccount__hiddenSide', {
+            [`${SHARED_CLASSES.outsideViewport}`]: this.state.isHiddenSideOutsideViewport,
+            [`${SHARED_CLASSES.collapsed}`]: this.state.isHiddenSideCollapsed
+        });
+
+        const loggedInHtml = (
+            <div className="myAccount">
+                <div className={hiddenSideClasses}
+                     ref={(c) => this.hiddenSide = c}
+                     style={{ width: this.state.hiddenSideWidth }}
+                >
+                    <a className="myAccount__link" href="/account/logout">Log out</a>
+                    <span className="myAccount__separator">-</span>
+                    <a className="myAccount__link is-active" href="/account">My Account</a>
+                </div>
+
+                <div className="myAccount__visibleSide">
+                    <a href="/account" className="myAccount__img">
+                        <div className="myAccount__gravatar">
+                            <img alt="avatar" src={`//gravatar.com/avatar/${md5(this.state.customer.email)}?s=40&d=blank`} />
+                        </div>
+                        <svg className="myAccount__icon">
+                            <use xlinkHref="#user-2" />
+                        </svg>
+                    </a>
+
+                    <a className="myAccount__link is-active"
+                       ref={(c) => this.welcomeMessage = c}
+                       style={{ width: this.state.welcomeMessageWidth }}
+                       href="/account"
+                    > Hi, {this.state.customer.first_name} !</a>
+                </div>
             </div>
         );
 
         const loggedOutHtml = (
-            <div className="myAccount__visibleSide">
-                <a href="/account/register" className="myAccount__img">
-                    <svg className="myAccount__icon">
-                        <use xlinkHref="#user-1"></use>
-                    </svg>
-                </a>
+            <div className="myAccount">
+                <div className={hiddenSideClasses}
+                     ref={(c) => this.hiddenSide = c}
+                     style={{ width: this.state.hiddenSideWidth }}
+                >
+                    <a className="myAccount__link" href="/account/login">Log in</a>
+                    {this.state.shop.customer_accounts_optional ?
+                        (
+                        <span>
+                            <span className="myAccount__separator"> - </span>
+                            <a className="myAccount__link is-active" href="/account/register">Register</a>
+                        </span>
+
+                        ) : ''
+                    }
+                </div>
+                <div className="myAccount__visibleSide">
+                    <a href="/account/register" className="myAccount__img">
+                        <svg className="myAccount__icon">
+                            <use xlinkHref="#user-1" />
+                        </svg>
+                    </a>
+                </div>
             </div>
         );
 
-        return (
-            <div className="myAccount">
-                <div className="myAccount__hiddenSide is-outside-viewport">
-                </div>
-                {this.state.loggedIn ? loggedInHtml : loggedOutHtml}
-            </div>
-        );
+        return this.state.loggedIn ? loggedInHtml : loggedOutHtml;
     }
 }
